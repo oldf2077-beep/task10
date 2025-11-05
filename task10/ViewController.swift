a435789e7ae76cf906c0ee5f1cefa9d07ae1e847
Akote <akote>
2025-11-05 19:32:21 +0200

    Initial Commit

---
+++ task10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task10
+//
+//  Created by akote on 5.11.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
